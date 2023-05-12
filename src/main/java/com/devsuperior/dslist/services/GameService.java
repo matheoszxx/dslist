d611d4b07b6664865fd7a0526857bf7978869441
Matheus Marques <matheuszxx1@outlook.com>
2023-05-11 22:50:05 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -21,6 +23,19 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	//MÉTODO PARA BUSCAR POR ID
+	public GameDTO findById(Long id) {
+		//BUSQUEI DO BANCO DE DADOS O A ENTIDADE GAME
+		Game result = gameRepository.findById(id).get();
+		//CONVERTER UM OBJETO GAME EM UM OBJETO GAMEDTO
+		GameDTO dto = new GameDTO(result);
+		return dto;	
+	}
+	
+	//ESSA ANOTATION É UMA BOA PRATICA DE PROGRAMAÇÃO, QUE GARANTE QUE A OPERAÇÃO COM O BANCO DE DADOS
+	//VAI ACONTECER RESPEITANDO OS PRINCIPIOS DAS TRANSAÇÕES 
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		//GERA UMA CONSULTA NO BANCO DE DADOS
 		List<Game> result = gameRepository.findAll();
